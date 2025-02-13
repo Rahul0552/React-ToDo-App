e350420b3f86a44f03c93795ade92d743aeb79a8
Rahul0552 <dharmasoth.rahul@gmail.com>
2025-02-13 17:13:10 +0530

    Made some changes ToDo APP

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base:/"Rahul0552.git.io/ToDo-APP"/,
+  base:"/Rahul0552.github.io/React-ToDo-App"
 })
